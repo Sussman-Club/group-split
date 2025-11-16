d517e7ada1985b3892fe82c330fd4ae1eb981086
Anabel Benítez González <anabelbenitezglez@gmail.com>
2025-11-16 17:17:57 -0500

    Add initial Blazor WebAssembly project structure

--- GroupSplit.AppHost/AppHost.cs
+++ GroupSplit.AppHost/AppHost.cs
@@ -2,5 +2,8 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<GroupSplit_API>("API");
+var backend = builder.AddProject<GroupSplit_API>("API");
+
+builder.AddProject<GroupSplit_Web>("Web").WithReference(backend);
+
 builder.Build().Run();
