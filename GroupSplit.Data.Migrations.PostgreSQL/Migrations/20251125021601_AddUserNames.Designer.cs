37dd8cba960e3814aaadf9c9a8a897196f365319
Omar Rivero <omarrivgw@gmail.com>
2025-11-24 21:36:36 -0500

    feat: add FirstName and LastName to User, Name to Group, and enhance Transaction with DateTime, Description, and Name properties

--- GroupSplit.Data.Migrations.PostgreSQL/Migrations/20251125021601_AddUserNames.Designer.cs
+++ GroupSplit.Data.Migrations.PostgreSQL/Migrations/20251125021601_AddUserNames.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GroupSplit.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GroupSplit.Data.Migrations.PostgreSQL.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20251125021601_AddUserNames")]
+    partial class AddUserNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GroupSplit.Data.Migrations.PostgreSQL/Migrations/20251125021601_AddUserNames.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GroupSplit.Data.Migrations.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "User",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "User",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Group",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaction_DateTime",
+                table: "Transaction",
+                column: "DateTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaction_Name",
+                table: "Transaction",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Group_Name",
+                table: "Group",
+                column: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Transaction_DateTime",
+                table: "Transaction");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transaction_Name",
+                table: "Transaction");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Group_Name",
+                table: "Group");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Group");
+        }
+    }
+}
