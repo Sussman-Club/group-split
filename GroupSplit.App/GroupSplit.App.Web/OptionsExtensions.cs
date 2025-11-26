b442ca3b8ffcfeffd42dc10f44f4d7ea7b4fdae4
Daniel Rivero <danielrivgw@gmail.com>
2025-11-26 17:38:27 -0500

    Adding redirect to login for every path that is not /api

--- GroupSplit.App/GroupSplit.App.Web/OptionsExtensions.cs
+++ GroupSplit.App/GroupSplit.App.Web/OptionsExtensions.cs
@@ -11,8 +11,17 @@ public static class OptionsExtensions
             // Prevent redirect on 401
             options.Events.OnRedirectToLogin = ctx =>
             {
-                ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                if (ctx.Request.Path.StartsWithSegments("/api"))
+                {
+                    ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    return Task.CompletedTask;
+                }
+
+                var returnUrl = Uri.EscapeDataString(ctx.Request.Path + ctx.Request.QueryString.ToString());
+                var loginUrl = $"/login?returnUrl={returnUrl}";
+                ctx.Response.Redirect(loginUrl);
                 return Task.CompletedTask;
+
             };
 
             // Prevent redirect on 403
