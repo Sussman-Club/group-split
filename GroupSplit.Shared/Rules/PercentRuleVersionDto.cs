d9a8d87b25a4ce26430a82a6fa323ac4cdb5fb34
Anabel Benitez <anabelbenitezglez@gmail.com>
2025-12-10 20:04:52 -0500

    Remove `NonNegativeValuesAttribute` and its usage in `RuleVersionDto` classes.

--- GroupSplit.Shared/Rules/PercentRuleVersionDto.cs
+++ GroupSplit.Shared/Rules/PercentRuleVersionDto.cs
@@ -1,9 +1,6 @@
-﻿using GroupSplit.Shared.CustomValidationAttributes;
-
-namespace GroupSplit.Shared;
+﻿namespace GroupSplit.Shared;
 
 public record PercentRuleVersionDto : RuleVersionDto
 {
-    [NonNegativeValues(ErrorMessage = "Percentages must be non-negative.")]
     public Dictionary<Guid, decimal> Percentages { get; init; } = new();
 }
--- GroupSplit.Shared/Rules/SharesRuleVersionDto.cs
+++ GroupSplit.Shared/Rules/SharesRuleVersionDto.cs
@@ -1,9 +1,6 @@
-﻿using GroupSplit.Shared.CustomValidationAttributes;
-
-namespace GroupSplit.Shared;
+﻿namespace GroupSplit.Shared;
 
 public record SharesRuleVersionDto : RuleVersionDto
 {
-    [NonNegativeValues(ErrorMessage = "Shares must be non-negative.")]
     public Dictionary<Guid, int> Shares { get; init; } = new();
 }
